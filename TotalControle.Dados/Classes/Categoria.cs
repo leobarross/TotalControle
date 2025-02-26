66367acbeeaa7b311feb276daa861e81438388c8
leobarross <leoolliver77@gmail.com>
2025-02-25 23:57:23 -0300

    Criado projeto de Dados

---
+++ TotalControle.Dados/Classes/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalControle.Dados.Classes
+{
+    public class Categoria
+    {
+        public Categoria()
+        {
+            Id = Guid.NewGuid().ToString();
+            
+        }
+
+        public string Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
